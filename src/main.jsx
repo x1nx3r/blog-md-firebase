f846c3d318c237b375fdb104c586dde3f64f0431
Mega Nugraha <monmega110@gmail.com>
2025-02-26 00:39:57 +0700

    fix typos

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import PostView from "./Postview";
+import PostView from "./PostView";
 
 const root = document.getElementById("root");
 
