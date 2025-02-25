ded2d69cee749c2c9070c1c04d98f5dfc8c996bb
Mega Nugraha <monmega110@gmail.com>
2025-02-26 00:43:51 +0700

    removing debug messages

--- src/utils/firebaseInit.js
+++ src/utils/firebaseInit.js
@@ -7,20 +7,6 @@ import {
   getDocs,
 } from "firebase/firestore";
 
-// Debugging logs to check environment variables
-console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("Firebase Auth Domain:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-console.log("Firebase Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
-console.log(
-  "Firebase Storage Bucket:",
-  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-);
-console.log(
-  "Firebase Messaging Sender ID:",
-  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-);
-console.log("Firebase App ID:", import.meta.env.VITE_FIREBASE_APP_ID);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
